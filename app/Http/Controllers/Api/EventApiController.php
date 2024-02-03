d0900700f333fdcaf1de4c6e7e1cda70d9ddd96b
Andre Zaharias <derandre3000@hotmail.com>
2024-02-03 14:37:40 +0100

    add more data to api calls

--- app/Http/Controllers/Api/EventApiController.php
+++ app/Http/Controllers/Api/EventApiController.php
@@ -27,6 +27,6 @@ class EventApiController extends Controller
      */
     public function show(Event $event)
     {
-        return new EventApiResource($event);
+        return new EventApiResource($event->load('group'));
     }
 }
--- app/Http/Resources/EventApiResource.php
+++ app/Http/Resources/EventApiResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,9 +19,13 @@ class EventApiResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'start' => $this->start,
-            'end' => $this->end,
+            'start' => $this->start->timestamp,
+            'end' => $this->end->timestamp,
+            'start_readable' => $this->start->format('Y-m-d'),
+            'end_readable' => $this->end->format('Y-m-d'),
             'description' => $this->description,
+            'interested' => $this->belongsToMany(User::class)->wherePivot('status', Event::$interested)->count(),
+            'attending' => $this->belongsToMany(User::class)->wherePivot('status', Event::$attending)->count(),
             'links' => [
                 'web' => route('events.show', $this),
                 'api' => route('api-events.show', $this),
